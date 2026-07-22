e4d6ecafb1bb9e7dfb5a2df3bca8f1266851f65a
kron0zzz <juanriosr7526@gmail.com>
2026-07-21 19:56:59 -0500

    feat: establecer base para informacion del pedido

---
+++ src/features/orders/components/OrderWorkspace/tabs/InfoTab/InfoTab.jsx
@@ -0,0 +1,14 @@
+import "./InfoTab.css";
+
+const InfoTab = () => {
+
+
+
+    return(
+        <div>
+            <p>Tab de información global en construccion</p>
+        </div>
+    );
+};
+
+export default InfoTab;
